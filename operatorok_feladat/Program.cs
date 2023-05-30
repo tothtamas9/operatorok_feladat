58744771ae8ea70134bb58d847230c8255fad564
tothtamas9 <ttamas026@gmail.com>
2023-05-30 18:07:11 +0200

    második commit

--- operatorok_feladat/Program.cs
+++ operatorok_feladat/Program.cs
@@ -4,24 +4,41 @@ namespace operatorok_feladat
 {
     internal class Program
     {
-        static List<string> adatok;
-        static List<string> maradekos;
+        static List<string> muveletek;
         static void Main(string[] args)
         {
             // 1. feladat
-            adatok = File.ReadAllLines("kifejezesek.txt").ToList();
+            muveletek = File.ReadAllLines("kifejezesek.txt").ToList();
             // mindenAdat.ForEach(x => Console.WriteLine(x));
 
             // 2. feladat
-            Console.WriteLine($"2. feladat: Kifejezések száma: {adatok.Count()}");
+            Console.WriteLine($"2. feladat: Kifejezések száma: {muveletek.Count()}");
 
             // 3.feladat
-            maradekos = adatok.Where(x => x.Contains("mod")).ToList();
+            int darab = muveletek.Count(x => x.Contains("mod"));
             //maradekos.ForEach(x => Console.WriteLine(x));
-            Console.WriteLine($"3. feladat: Kifejezések maradékos osztással: {maradekos.Count()}");
+            Console.WriteLine($"3. feladat: Kifejezések maradékos osztással: {darab}");
 
             // 4.feladat
+            bool vanIlyen = false;
 
+            foreach (var sor in muveletek)
+            {
+                var tomb = sor.Split();
+                if (Convert.ToInt32(tomb[0])%10==0 && Convert.ToInt32(tomb[2]) % 10 == 0)
+                {
+                    vanIlyen = true;
+                    break;
+                }
+            }
+            if (vanIlyen)
+            {
+                Console.WriteLine("van");
+            }
+            else
+            {
+                Console.WriteLine("nincs");
+            }
 
         }
     }

cfabf5e2dfbceb5913405b0a782a6194b08cb5a2
tothtamas9 <ttamas026@gmail.com>
2023-05-30 17:30:20 +0200

    elso commit

--- operatorok_feladat/Program.cs
+++ operatorok_feladat/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace operatorok_feladat
+﻿using System.Threading.Channels;
+
+namespace operatorok_feladat
 {
     internal class Program
     {
+        static List<string> adatok;
+        static List<string> maradekos;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 1. feladat
+            adatok = File.ReadAllLines("kifejezesek.txt").ToList();
+            // mindenAdat.ForEach(x => Console.WriteLine(x));
+
+            // 2. feladat
+            Console.WriteLine($"2. feladat: Kifejezések száma: {adatok.Count()}");
+
+            // 3.feladat
+            maradekos = adatok.Where(x => x.Contains("mod")).ToList();
+            //maradekos.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine($"3. feladat: Kifejezések maradékos osztással: {maradekos.Count()}");
+
+            // 4.feladat
+
+
         }
     }
 }
